6ed1e4ff784c461924ecd5fd73d379210d8bc8d5
Sergey Koshelnikov <koshelnikovsa@gmail.com>
2016-06-16 20:13:36 +0300

    Initial commit

---
+++ ExampleLibrary/ExampleClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleLibrary
+{
+    public class ExampleClass
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ReSharperTestExample/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.QualityTools.Testing.Fakes;
+using Xunit;
+
+namespace ReSharperTestExample
+{
+
+    public class UnitTest
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            using (ShimsContext.Create())
+            {
+                Assert.True(true);
+            }
+        }
+    }
+}
